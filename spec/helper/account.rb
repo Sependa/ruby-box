4b71b1040feba1eddb22b119733f8c2e5678b16d
Filip Tepper <filip@tepper.pl>
2015-01-19 11:15:19 +0100

    remove const already defined warning

--- spec/helper/account.rb
+++ spec/helper/account.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 
-ACCOUNT = {}
 begin
   ACCOUNT = YAML.load_file(File.dirname(__FILE__) + '/account.yml')
 rescue
+  ACCOUNT = {}
   p "create an account.yml file with your credentials to run integration tests."
 end
